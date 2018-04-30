7d7c52db61358d91bcb80fd3dcddad6ce28b07c0
sagar15 <sagar@finday.com>
2018-04-30 14:23:04 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LeavemanagementComponent } from './leavemanagement/leavemanagement.component';
+import { EssportalComponent } from './essportal/essportal.component';
+import { LoginComponent } from './login/login.component';
+import { TimesheetsComponent } from './timesheets/timesheets.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LeavemanagementComponent,
+    EssportalComponent,
+    LoginComponent,
+    TimesheetsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'leave', component: LeavemanagementComponent},
+      {path: 'ess', component: EssportalComponent},
+      {path: 'timesheets', component: TimesheetsComponent},
+      {path: 'login', component: LoginComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/essportal/essportal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-essportal',
+  templateUrl: './essportal.component.html',
+  styleUrls: ['./essportal.component.css']
+})
+export class EssportalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
