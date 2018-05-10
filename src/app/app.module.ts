98568d4080ee1bb829ac2f4643cfbb5fd876564e
sagar15 <sagar@finday.com>
2018-05-10 12:50:53 +0530

    new with css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http'
 
 
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { TimesheetsComponent } from './timesheets/timesheets.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
       {path: 'leave', component: LeavemanagementComponent},
---
+++ src/app/leavemanagement/applyleave/applyleave.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applyleave',
+  templateUrl: './leavemanagement/applyleave/applyleave.component.html',
+  styleUrls: ['./leavemanagement/applyleave/applyleave.component.css']
+})
+export class ApplyLeaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
